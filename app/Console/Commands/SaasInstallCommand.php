f3ece5542570aa5d509b155904564b93a8f7274e
mouyong <mouyong@users.noreply.github.com>
2022-09-07 16:41:23 +0800

    Update SaasInstallCommand.php

--- app/Console/Commands/SaasInstallCommand.php
+++ app/Console/Commands/SaasInstallCommand.php
@@ -393,6 +393,21 @@ foreach (config('tenancy.central_domains', []) as \$domain) {
             ]);
             \Illuminate\Support\Facades\URL::forceScheme('https');
         }
+
+        \Illuminate\Database\Eloquent\Builder::macro('result', \$paginate = function () {
+            \$namespace = request()->route()->getAction('namespace');
+
+            if (!request('page') || request('export')) {
+                return $this->get(request('columns', ['*']));
+            }
+
+            if (\Illuminate\Support\Str::contains(\$namespace, 'App\Http\Controllers\Admin')) {
+                return $this->paginate(request('per_page', 20) <= 100 ? request('per_page', 20) : 100, request('columns', ['*']));
+            }
+
+            return $this->paginate(request('per_page', 20) <= 100 ? request('per_page', 20) : 100, request('columns', ['*']));
+        });
+        \Illuminate\Database\Query\Builder::macro('result', \$paginate);
     }
 ",
             ],
